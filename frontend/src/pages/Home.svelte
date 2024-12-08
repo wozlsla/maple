4693377cfad0a587f654607bca86b25da3dd26c6
jimin <jiminseo0307@gmail.com>
2024-12-08 14:24:14 +0900

    core(vscode): add prettier configuration

--- frontend/src/pages/Home.svelte
+++ frontend/src/pages/Home.svelte
@@ -1,19 +1,19 @@
 <script>
-    let posts_list = []; // init
+  let posts_list = []; // init
 
-    function get_posts() {
-        fetch("http://127.0.0.1:8000/api/board/list").then((response) => {
-            response.json().then((json) => {
-                posts_list = json;
-            });
-        });
-    }
+  function get_posts() {
+    fetch('http://127.0.0.1:8000/api/board/list').then((response) => {
+      response.json().then((json) => {
+        posts_list = json;
+      });
+    });
+  }
 
-    get_posts();
+  get_posts();
 </script>
 
 <ul>
-    {#each posts_list as post}
-        <li>{post.title}</li>
-    {/each}
+  {#each posts_list as post}
+    <li>{post.title}</li>
+  {/each}
 </ul>
